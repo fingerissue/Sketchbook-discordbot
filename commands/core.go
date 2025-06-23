dd17d804c22850751bd6e917a0ec223ad6fd2968
fingerissue <fingerissue@gmail.com>
2025-06-24 01:40:53 +0900

    Finished 3-Refactoring Journey

--- commands/core.go
+++ commands/core.go
@@ -1,1 +1,12 @@
 package commands
+
+import (
+	"github.com/bwmarrin/discordgo"
+)
+
+func OnInteractionCreate(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	switch i.ApplicationCommandData().Name {
+	case "ping":
+		handlePing(s, i)
+	}
+}
--- commands/ping.go
+++ commands/ping.go
@@ -5,14 +5,12 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-func OnInteractionCreate(s *discordgo.Session, i *discordgo.InteractionCreate) {
+func handlePing(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	ping := s.HeartbeatLatency().Milliseconds()
-	if i.ApplicationCommandData().Name == "ping" {
-		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: fmt.Sprintf("🏓 Pong! %dms", ping),
-			},
-		})
-	}
+	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: fmt.Sprintf("🏓 Pong! %dms", ping),
+		},
+	})
 }
