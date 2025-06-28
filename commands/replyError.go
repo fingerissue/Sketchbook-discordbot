d80ee4886805cc19aeb5c685dde6304ad59894d0
fingerissue <fingerissue@gmail.com>
2025-06-28 17:02:51 +0900

    Add error function Interaction and Followup

--- commands/replyError.go
+++ commands/replyError.go
@@ -5,12 +5,28 @@ import (
 	"log"
 )
 
-func replyError(s *discordgo.Session, i *discordgo.InteractionCreate, msg string) {
-	_, err := s.FollowupMessageCreate(i.Interaction, true, &discordgo.WebhookParams{
-		Content: msg,
+func replyErrorInteraction(s *discordgo.Session, i *discordgo.InteractionCreate, msg string) {
+	err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: msg,
+		},
 	})
 
 	if err != nil {
-		log.Println("FollowupMessageCreate error: ", err)
+		log.Println("While error message sending error: ", err)
+	}
+}
+
+func replyErrorFollowup(s *discordgo.Session, i *discordgo.InteractionCreate, msg string) {
+	err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: msg,
+		},
+	})
+
+	if err != nil {
+		log.Println("While error message sending error: ", err)
 	}
 }
