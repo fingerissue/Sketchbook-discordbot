22673c721f4e47938a687e8532a045532e3fe5bb
fingerissue <fingerissue@gmail.com>
2025-07-03 21:17:55 +0900

    Update 4-DataBase Connection

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"Sketchbook/commands"
+	"database/sql"
 	"fmt"
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
@@ -9,6 +10,8 @@ import (
 	"os"
 	"os/signal"
 )
+
+import _ "github.com/go-sql-driver/mysql"
 
 func main() {
 	err := godotenv.Load(".env")
@@ -19,6 +22,26 @@ func main() {
 	DiscordBotToken := os.Getenv("DISCORD_BOT_TOKEN")
 	if DiscordBotToken == "" {
 		log.Fatal("You need to set DISCORD_BOT_TOKEN")
+	}
+
+	DatabaseAddress := os.Getenv("DB_ADDR")
+	if DatabaseAddress == "" {
+		log.Fatal("You need to set DB_ADDR")
+	}
+
+	DatabaseID := os.Getenv("DB_ID")
+	if DatabaseID == "" {
+		log.Fatal("You need to set DB_ID")
+	}
+
+	DatabasePW := os.Getenv("DB_PW")
+	if DatabasePW == "" {
+		log.Fatal("You need to set DB_PW")
+	}
+
+	DatabaseNAME := os.Getenv("DB_NAME")
+	if DatabaseNAME == "" {
+		log.Fatal("You need to set DB_NAME")
 	}
 
 	Sketchbook, err := discordgo.New("Bot " + DiscordBotToken)
@@ -73,6 +96,16 @@ func main() {
 			log.Fatal("Unable to create application command: ", err)
 		}
 	}
+
+	DB, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s", DatabaseID, DatabasePW, DatabaseAddress, DatabaseNAME))
+	if err != nil {
+		log.Fatal("Unable to connect database: ", err)
+	}
+	defer func() {
+		if err := DB.Close(); err != nil {
+			log.Fatal("Unable to close database: ", err)
+		}
+	}()
 
 	fmt.Println("Bot is now running. Press CTRL-C to exit.")
 
